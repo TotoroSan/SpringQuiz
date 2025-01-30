e896ff13da47efdd5351e59e99b31fb0e0852296
TotoroSan <22733446+Totorosan@users.noreply.github.com>
2025-01-30 20:17:28 +0100

    added ShopGameEvent Skeleton

---
+++ SpringQuiz/src/main/java/com/example/quiz/model/dto/ShopGameEventDto.java
@@ -0,0 +1,5 @@
+package com.example.quiz.model.dto;
+
+public class ShopGameEventDto {
+    // todo DTO for a GameEvent object
+}
---
+++ SpringQuiz/src/main/java/com/example/quiz/model/entity/ShopGameEvent.java
@@ -0,0 +1,5 @@
+package com.example.quiz.model.entity;
+
+public class ShopGameEvent {
+    // todo palceholder for shop game event
+}
